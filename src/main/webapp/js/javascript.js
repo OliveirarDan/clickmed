4d1364e28b61981a8a4a82749fb5bcd58974a61d
Estevao Lucena <estevao.llucena@gmail.com>
2019-04-30 23:24:46 -0300

    Finalização dos campos de cadastro e adição de informações à direita na pagina

--- src/main/webapp/js/javascript.js
+++ src/main/webapp/js/javascript.js
@@ -3,8 +3,7 @@
  */
 
 $(document).ready(function() {
-	
-	
+
 	/* Chamada e configurações do Steps e Validate */
 	var form = $("#cadastro-medico");
 
@@ -12,42 +11,36 @@ $(document).ready(function() {
 		headerTag : "h2",
 		bodyTag : "section",
 		transitionEffect : "slideLeft",
-		onStepChanging: function (event, currentIndex, newIndex)
-		{
+		onStepChanging : function(event, currentIndex, newIndex) {
 			form.validate().settings.ignore = ":disabled,:hidden";
 			return form.valid();
 		},
-		onFinishing: function (event, currentIndex)
-		{
+		onFinishing : function(event, currentIndex) {
 			form.validate().settings.ignore = ":disabled";
 			return form.valid();
-		},
-//		onFinished: function (event, currentIndex)
-//		{
-//			alert("Submitted!");
-//		}
+		}
 	});
-	
+
 	/* Chamada do validate e regras de cada campo e mensagens personalizadas. */
 	$("#cadastro-medico").validate({
-		rules: {
-			email: {
-				required: true,
-				email: true,
+		rules : {
+			email : {
+				required : true,
+				email : true,
 			},
-			cpf: {
-				cpf: true, 
-				required: true
+			cpf : {
+				cpf : true,
+				required : true
 			}
 		},
-		messages: {		
-			cpf: { 
-				cpf: 'CPF inválido'
+		messages : {
+			cpf : {
+				cpf : 'CPF inválido'
 			}
-	      }
+		}
 
 	});
-	
+
 	/* Selectize: criação dos arrays dos campos */
 	$('#diasAtendimento').selectize({
 		maxItems : null,
@@ -79,30 +72,23 @@ $(document).ready(function() {
 		create : false
 	});
 
-	$('#localAtendimento').selectize(
-			{
-				maxItems : 1,
-				valueField : 'title',
-				labelField : 'title',
-				searchField : 'title',
-				options : [
-						{
-							id : 1,
-							title : 'Hospital das Clinicas' + ' - '
-									+ 'Av Nações Unidas, 7221'
-						},
-						{
-							id : 2,
-							title : 'Hospital das Clinicas' + ' - '
-									+ 'Av Nações Unidas, 7221'
-						},
-						{
-							id : 3,
-							title : 'Hospital das Clinicas' + ' - '
-									+ 'Av Nações Unidas, 7221'
-						} ],
-				create : false
-			});
+	$('#localAtendimento').selectize({
+		maxItems : 1,
+		valueField : 'title',
+		labelField : 'title',
+		searchField : 'title',
+		options : [ {
+			id : 1,
+			title : 'Hospital das Clinicas' + ' - ' + 'Av Nações Unidas, 7221'
+		}, {
+			id : 2,
+			title : 'Hospital das Clinicas' + ' - ' + 'Av Nações Unidas, 7221'
+		}, {
+			id : 3,
+			title : 'Hospital das Clinicas' + ' - ' + 'Av Nações Unidas, 7221'
+		} ],
+		create : false
+	});
 
 	$('#convenios').selectize({
 		maxItems : null,
@@ -138,9 +124,45 @@ $(document).ready(function() {
 			title : 'Psicólogo'
 		} ],
 
-		onChange: function(value) {
+		onChange : function(value) {
 			$('#especialidade').attr('value', value);
-	    },
+		},
 		create : false
 	});
+
+	/*// Executa a requisição quando o campo cpf perder o foco
+	$('#cpf').blur(function() {
+		var cpf = $('#cpf').val().replace(/[^0-9]/g, '').toString();
+
+		if (cpf.length == 11) {
+			var v = [];
+
+			// Calcula o primeiro dígito de verificação.
+			v[0] = 1 * cpf[0] + 2 * cpf[1] + 3 * cpf[2];
+			v[0] += 4 * cpf[3] + 5 * cpf[4] + 6 * cpf[5];
+			v[0] += 7 * cpf[6] + 8 * cpf[7] + 9 * cpf[8];
+			v[0] = v[0] % 11;
+			v[0] = v[0] % 10;
+
+			// Calcula o segundo dígito de verificação.
+			v[1] = 1 * cpf[1] + 2 * cpf[2] + 3 * cpf[3];
+			v[1] += 4 * cpf[4] + 5 * cpf[5] + 6 * cpf[6];
+			v[1] += 7 * cpf[7] + 8 * cpf[8] + 9 * v[0];
+			v[1] = v[1] % 11;
+			v[1] = v[1] % 10;
+
+			// Retorna Verdadeiro se os dígitos de verificação são os esperados.
+			if ((v[0] != cpf[9]) || (v[1] != cpf[10])) {
+				alert('CPF inválido: ' + cpf);
+
+				$('#cpf').val('');
+				$('#cpf').focus();
+			}
+		} else {
+			alert('CPF inválido:' + cpf);
+
+			$('#cpf').val('');
+			$('#cpf').focus();
+		}
+	});*/
 });

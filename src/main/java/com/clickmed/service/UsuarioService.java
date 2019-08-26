5e7c11e4017d1b4aef16c363c2ad038b19b7d872
Estevao Lucena <estevao.llucena@gmail.com>
2019-08-26 00:21:43 -0300

    API Usuario Auth

--- src/main/java/com/clickmed/service/UsuarioService.java
+++ src/main/java/com/clickmed/service/UsuarioService.java
@@ -77,7 +77,9 @@ public class UsuarioService {
 	
 	public Usuario autenticar(Usuario usuario) {
 		Usuario usuarioAutenticado = usuarioDAO.findByEmail(usuario.getEmail());
+
 		if (usuarioAutenticado.getSenha().equals(usuario.getSenha())) {
+
 			return usuarioAutenticado;
 		}
 		return null;
